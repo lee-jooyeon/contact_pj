bd19ab507106add827c309ac5e57b0e49c051eb1
Aspyn0925 <Aspyn@plgrim.com>
2023-04-11 17:42:42 +0900

    Feat Navbar 뒤로가기 기능 추가

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,16 @@
 import { MdOutlineArrowBackIosNew, MdAdd } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
 export default function NavBar() {
+  const navigate = useNavigate();
+
+  const NavigateBack = () => {
+    navigate(-1);
+  }
+
   return (
     <nav className='py-4 px-2.5 flex justify-between'>
-      <span className='self-center'>
+      <span className='self-center' onClick={NavigateBack}>
         {/* 맨처음 렌더링 됐을 땐 안 보였다가 다른 페이지 랜딩했을 때 보여주기 */}
         <MdOutlineArrowBackIosNew className='text-white w-5 h-5' />
       </span>
