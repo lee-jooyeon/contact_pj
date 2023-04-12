77915536491ebfd921351acb609f4a1d48f3b04b
Aspyn0925 <Aspyn@plgrim.com>
2023-04-12 17:54:11 +0900

    Fix firebase안에 fetch data 함수, useQuery 수정하기

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,27 +1,15 @@
-import { useEffect, useState } from "react";
-import {  collection, getDocs } from "firebase/firestore";
-import { db } from "../../firebase";
+import { useQuery } from "@tanstack/react-query";
+import { getData } from "../../firebase";
 import ContactItem from "../ContactItem";
 
 export default function Contact(){
-  const [lists, setLists] = useState([]);
+  const { isLoading, error, data: contacts } = useQuery(['contacts'], getData);
 
-  useEffect(() => {
-    const FetchData = async () => {
-      await getDocs(collection(db,'contacts'))
-        .then((querySnapshot) => {
-          const contactData = querySnapshot.docs.map((doc) => ({...doc.data(), id: doc.id}));
-          setLists(contactData);
-          console.log(lists, contactData);
-      });
-    }
-    FetchData();
-   }, []);
-
+  console.log(contacts && contacts)
 
   return(
     <ul>
-      {lists?.map((data) => <ContactItem key={data.id} data={data} />)}
+      {/* {lists?.map((data) => <ContactItem key={data.id} data={data} />)} */}
     </ul>
   )
 }
--- src/components/pages/Root.jsx
+++ src/components/pages/Root.jsx
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../NavBar';
 
-export default function Layout() {
+const queryClient = new QueryClient();
+
+export default function Root() {
   return (
     <div>
       <NavBar />
       {/* 해당하는 부분만 라우팅하고 싶다면 (변경하고싶다면) 원하는 자식 루트는 outlet에다가 위치할 수 있다 */}
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </div>
   );
 }
