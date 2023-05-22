c4488438adda02220b84c8eeeffc038ce0ae5373
Aspyn0925 <Aspyn@plgrim.com>
2023-05-22 17:52:32 +0900

    fix: root 페이지에 navbar 삭제

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,16 @@
 import { MdOutlineArrowBackIosNew, MdAdd } from 'react-icons/md';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function NavBar() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const NavigateBack = () => {
     navigate(-1);
+  }
+
+  if(location.pathname === '/'){
+    return null;
   }
 
   return (
@@ -14,7 +19,7 @@ export default function NavBar() {
         {/* 맨처음 렌더링 됐을 땐 안 보였다가 다른 페이지 랜딩했을 때 보여주기 */}
         <MdOutlineArrowBackIosNew className='text-white w-5 h-5' />
       </span>
-      <div className='leading-8 font-medium text-white self-center'>
+      <div className='leading-8 font-medium text-white self-center' onClick={() => navigate('/')}>
         Contacts
       </div>
       <span className='self-center' onClick={() => navigate('/newcontact')}>
--- src/components/pages/Root.jsx
+++ src/components/pages/Root.jsx
@@ -6,11 +6,12 @@ import NavBar from '../NavBar';
 const queryClient = new QueryClient();
 
 export default function Root() {
+
   return (
     <div>
-      <NavBar />
       {/* 해당하는 부분만 라우팅하고 싶다면 (변경하고싶다면) 원하는 자식 루트는 outlet에다가 위치할 수 있다 */}
       <QueryClientProvider client={queryClient}>
+        <NavBar />  
         <Outlet />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
