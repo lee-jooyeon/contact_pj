1632610c8b2010d8bc03d9bca89ce5d40f8186be
Aspyn0925 <Aspyn@plgrim.com>
2023-05-31 11:39:04 +0900

    fix: 메인 홈에서 navbar 안 보이도록 수정

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,14 +15,14 @@ export default function NavBar() {
 
   return (
     <nav className='py-4 px-2.5 flex justify-between'>
-      <span className='self-center' onClick={NavigateBack}>
+      <span className='self-center cursor-pointer' onClick={NavigateBack}>
         {/* 맨처음 렌더링 됐을 땐 안 보였다가 다른 페이지 랜딩했을 때 보여주기 */}
         <MdOutlineArrowBackIosNew className='text-white w-5 h-5' />
       </span>
       <div className='leading-8 font-medium text-white self-center' onClick={() => navigate('/')}>
         Contacts
       </div>
-      <span className='self-center' onClick={() => navigate('/newcontact')}>
+      <span className='self-center cursor-pointer' onClick={() => navigate('/newcontact')}>
         <MdAdd className='text-white w-8 h-8' />
       </span>
     </nav>
