bee9f399b355b1da94d77b2db6484e210c5570e0
Aspyn0925 <Aspyn@plgrim.com>
2023-05-31 11:39:25 +0900

    fix: contact list UI 수정

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -6,10 +6,10 @@ export default function ContactItem({ list }) {
 
   return (
     <li
-      className='relative flex py-4 mx-4 text-white'
+      className='relative flex py-4 mx-4 text-white cursor-pointer'
       onClick={() => navigate(`/contacts/${id}`, { state: { list } })}
     >
-      <span className='text-black bg-[#eeeeee] inline-block w-14 h-14 text-center leading-[3.5rem] rounded-full text-sm'>
+      <span className='text-black bg-[#eeeeee] inline-block w-[4.5rem] h-[4.5rem] text-center leading-[4.5rem] rounded-full text-sm'>
         {name}
       </span>
       <span className='pl-4 self-center text-white text-sm flex-1 mb-5'>
@@ -23,7 +23,7 @@ export default function ContactItem({ list }) {
             ? 'bg-[#65a0d7]'
             : 'bg-[#d587dd]'
         }
-      mr-2.5 absolute self-center left-[4.2rem] top-12 w-14 text-[10px] text-center text-white uppercase leading-4 rounded-full`}
+      mr-2.5 absolute self-center left-[5.3rem] top-[3.6rem] w-14 text-[10px] text-center text-white uppercase leading-4 rounded-full`}
       >
         {group}
       </div>
