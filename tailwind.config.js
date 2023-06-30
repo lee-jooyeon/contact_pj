c1a0eb0d5141ef2d18ee41f8a5ad058f95f4e284
Aspyn0925 <Aspyn@plgrim.com>
2023-06-30 13:39:32 +0900

    fix: css 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        NanumSquare: ['SpoqaHanSans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
