a4b1fe5784b6ff4412789ac6c7e2dc2d8823ae18
Aspyn0925 <Aspyn@plgrim.com>
2023-07-07 17:25:47 +0900

    feat: 로그인, 로그아웃 추가

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,13 +4,15 @@ import Loading from '../lottie/loading.json';
 import ContactItem from '../components/ContactItem';
 import ErrorPage from './ErrorPage';
 import Header from '../components/Header';
+import { FiLogOut } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import { getLists } from '../api/firebase';
+import useGetContact from '../hooks/queries/useGetContacts';
+import { useAuthContext } from '../components/context/AuthContext';
 
 export default function Contact() {
+  const {logout} = useAuthContext();
   const navigate = useNavigate();
-  const {isLoading, error, data} = useQuery(['useGetContact'], () => getLists(), {staleTime: 1000 * 60});
+  const {isLoading, error, data} = useGetContact();
 
   if (isLoading) return 
     <div className='mt-40'>
@@ -37,6 +39,7 @@ export default function Contact() {
       <ul>
         {data && data.map(data => <ContactItem key={data.id} list={data} />)}
       </ul>
+      <button onClick={logout}><FiLogOut className='absolute bottom-12 right-4 w-9 h-10 text-white' /></button>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../components/context/AuthContext';
 
 export default function Home() {
-  const navigate = useNavigate();
+  const {user, login} = useAuthContext();
   const publicUrl = process.env.PUBLIC_URL;
+  const navigate = useNavigate();
 
-  const onClickHandle = () => {
-    navigate(`/contacts`);
-  };
+  const useLogin = () => {
+    login().then(() => {
+      setTimeout(() => {
+        navigate('/contacts')
+      }, 500);
+    });
+  }
 
   return (
-    <div className='mt-40 mx-4 pb-4 text-center mt-'>
-      <img
-        src={`${publicUrl}/images/emoji_.png`}
-        alt='image'
-        className='w-24- h-40 mx-auto my-0'
-      />
-      <span className='block mt-4 text-xl text-white'>JOOYEON</span>
-      <button className='contact_btn' onClick={onClickHandle}>
-        CONTACTS
-      </button>
+    <div className='absolute left-2/4 top-2/4 -translate-x-1/2 -translate-y-1/2'>
+      {user ? <img src={user?.photoURL} alt='user' className='w-24- h-40 mx-auto my-0 rounded-full'/> :
+        <img
+          src={`${publicUrl}/images/emoji_.png`}
+          alt='image'
+          className='w-24- h-40 mx-auto my-0'
+        /> 
+      }
+      {!user && <button className='contact_btn' onClick={useLogin}>LOGIN</button>}
+      {user && <button className='contact_btn' onClick={() => navigate('/contacts')}>{user?.displayName}</button>}
     </div>
   );
 }
