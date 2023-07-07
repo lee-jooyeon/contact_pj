687d12dfdbaa0e4f38c51840e04941d0ca3f2e2a
Aspyn0925 <Aspyn@plgrim.com>
2023-07-07 17:23:10 +0900

    fix: header css 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { MdOutlineArrowBackIosNew, MdAdd } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
-export default function Header({headerLeft, headerRight, handleMain, handleLeftButton, handleRightButton}) {
+export default function Header({headerLeft, headerRight, handleLeftButton, handleRightButton}) {
+  const navigate = useNavigate();
+
   return (
     <header className='py-4 px-2.5 flex justify-center'>
       {headerLeft && (
@@ -9,7 +12,7 @@ export default function Header({headerLeft, headerRight, handleMain, handleLeftB
         <MdOutlineArrowBackIosNew className='text-white w-5 h-5' />
       </span>
       )}
-      <div className='leading-8 text-lg font-medium uppercase text-white self-center' onClick={handleMain}>
+      <div className='leading-8 text-lg font-medium uppercase font-semibold text-white self-center cursor-pointer' onClick={() => navigate('/')}>
         Contacts
       </div>
       {headerRight && (
